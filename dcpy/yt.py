1435548d8c005875b5a8c01518aa53db437a013f
IceTeaOxO <86127014+IceTeaOxO@users.noreply.github.com>
2022-08-14 03:20:37 +0800

    Add files via upload

---
+++ dcpy/yt.py
@@ -0,0 +1,21 @@
+from pytube import YouTube
+import os
+# import moviepy
+# from moviepy.editor import *
+
+
+url = 'https://www.youtube.com/watch?v=i-d-s01gIrs'
+def makeMusic(url):
+    yt = YouTube(url)
+    yt.streams.filter(only_audio=True)
+    stream = yt.streams.get_by_itag(251)
+    stream.download(output_path="./music")
+    #轉換成mp3
+    new_file = './music/'+stream.default_filename + '.mp3'
+    os.rename('./music/'+stream.default_filename, new_file)
+    return stream.default_filename
+# makeMusic(url)
+
+
+# video = VideoFileClip('Sora no Kiseki the 3rd Evolution [BGM RIP] - Cry for your Eternity.3gpp')
+# video.audio.write_audiofile('test.mp3')
